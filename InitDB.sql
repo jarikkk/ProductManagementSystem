348e2ca167f16e7fdeb68553d80a85d0afbc3305
yakovalenko <jaroslawkovalenko@jmail.com>
2017-02-09 15:43:20 +0200

    spring f

--- InitDB.sql
+++ InitDB.sql
@@ -1,63 +1,18 @@
-DROP SCHEMA IF EXISTS module_1;
+DROP DATABASE IF EXISTS PRODUCTMANAGEMENTSYSTEM;
 
-CREATE SCHEMA IF NOT EXISTS module_1;
+CREATE DATABASE PRODUCTMANAGEMENTSYSTEM;
 
-alter table if exists developers_skills drop constraint developers_skills_developer_id_fkey;
-alter table if exists developers_skills drop constraint developers_skills_skill_id_fkey;
-alter table if exists projects drop constraint projects_project_customer_id_fkey;
-alter table if exists projects drop constraint projects_project_company_id_fkey;
-alter table if exists developers drop constraint developers_developer_company_id_fkey;
-alter table if exists developers drop constraint developers_developer_project_id_fkey;
+DROP SCHEMA IF EXISTS module_4;
 
-drop table if exists SKILLS;
-drop table if exists CUSTOMERS;
-drop table if exists COMPANIES;
-drop table if exists PROJECTS;
-drop table if exists DEVELOPERS;
-drop table if exists DEVELOPERS_SKILLS;
+CREATE SCHEMA IF NOT EXISTS module_4;
 
-create table SKILLS(
-skill_id serial PRIMARY KEY NOT NULL,
-skill_name character varying NOT NULL
+DROP TABLE IF EXISTS GOODS;
+
+CREATE TABLE module_4.GOODS (
+  ID           SERIAL PRIMARY KEY NOT NULL,
+  PRODUCT_NAME CHARACTER VARYING  NOT NULL,
+  MANUFACTURER CHARACTER VARYING  NOT NULL,
+  PRISE        INTEGER            NOT NULL,
+  DESCRIPTION  CHARACTER VARYING  NOT NULL
 );
 
-
-create table CUSTOMERS(
-customer_id serial PRIMARY KEY NOT NULL,
-customer_name character varying NOT NULL
-);
-
-
-
-create table COMPANIES(
-companie_id serial PRIMARY KEY NOT NULL,
-companie_name character varying NOT NULL
-);
-
-
-
-create table PROJECTS(
-project_id serial  PRIMARY KEY NOT NULL,
-project_name character varying NOT NULL,
-project_company_id integer references  COMPANIES(companie_id),
-project_customer_id integer references CUSTOMERS(customer_id) NOT NULL,
-project_start_timestamp date NOT NULL
-);
-
-
-
-
-create table DEVELOPERS(
-developer_id serial PRIMARY KEY NOT NULL,
-developer_name character varying NOT NULL,
-developer_company_id integer references COMPANIES(companie_id) NOT NULL,
-developer_project_id integer references PROJECTS (project_id) NOT NULL,
-developer_join_date date
-);
-
-
-
-create table DEVELOPERS_SKILLS(
-  developer_id integer references DEVELOPERS(developer_id) NOT NULL,
-  skill_id  integer   references SKILLS(skill_id) NOT NULL
-);
