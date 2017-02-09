146046fddb0f0009be7dbd1fc914c6e4b6b8d54f
yakovalenko <jaroslawkovalenko@jmail.com>
2017-02-09 16:29:22 +0200

    model

--- src/main/java/dao/BookDao.java
+++ src/main/java/dao/BookDao.java
@@ -1,5 +1,14 @@
 package dao;
 
 
-public class BookDao {
+import model.Product;
+
+public interface BookDao {
+
+    public void addProduct(Product product);
+
+    public void updateProduct(Product product);
+
+    public void removeProduct(int id);
+
 }
