fc49aa8575aa6203e2aed6e173887519f55d008d
yakovalenko <jaroslawkovalenko@jmail.com>
2017-02-10 18:30:20 +0200

    .service

--- src/main/java/service/ProductServiceImpl.java
+++ src/main/java/service/ProductServiceImpl.java
@@ -1,31 +1,47 @@
 package service;
 
+import dao.ProductDao;
 import model.Product;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class ProductServiceImpl implements ProductService {
+
+    ProductDao productDao;
+
+    public void setProductDao(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
     @Override
+    @Transactional
     public void addProduct(Product product) {
 
     }
 
     @Override
+    @Transactional
     public void updateProduct(Product product) {
 
     }
 
     @Override
+    @Transactional
     public void removeProduct(int id) {
 
     }
 
     @Override
+    @Transactional
     public Product getProductId(int id) {
         return null;
     }
 
     @Override
+    @Transactional
     public List<Product> listProducts() {
         return null;
     }
