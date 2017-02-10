75dd9a35ab808e7e469178390672c16e8290a8b1
yakovalenko <jaroslawkovalenko@jmail.com>
2017-02-10 18:03:19 +0200

    .service

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,5 +1,18 @@
 package service;
 
 
+import model.Product;
+
+import java.util.List;
+
 public interface ProductService {
+    public void addProduct(Product product);
+
+    public void updateProduct(Product product);
+
+    public void removeProduct(int id);
+
+    public Product getProductId(int id);
+
+    public List<Product> listProducts();
 }
--- src/main/java/service/ProductServiceImpl.java
+++ src/main/java/service/ProductServiceImpl.java
@@ -1,4 +1,32 @@
 package service;
 
-public class ProductServiceImpl {
+import model.Product;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public void addProduct(Product product) {
+
+    }
+
+    @Override
+    public void updateProduct(Product product) {
+
+    }
+
+    @Override
+    public void removeProduct(int id) {
+
+    }
+
+    @Override
+    public Product getProductId(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> listProducts() {
+        return null;
+    }
 }
